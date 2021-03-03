d45296962d95a3fd8ce58d1a0f9e30b80910f9ee
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-03-03 10:09:06 -0500

    Add render Node helper

---
+++ src/helpers/renderNode.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const renderNode = (Component?: React.ReactNode) => {
+  if (!Component) {
+    return null
+  } else if (React.isValidElement(Component) || typeof Component !== 'function') {
+    return Component
+  } else {
+    return <Component />
+  }
+}
+
+export default renderNode
